04f47d238d8e8b83336d5e894bcb826843efe3ad
Ranami <nasyrov.ramir@mail.ru>
2022-06-26 20:31:08 +0600

    added styled for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,40 +2,35 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { styled } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
-const pages = ["Главное", "Коллекция", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const pages = [
+  {
+    name: "",
+    title: "Главное",
+  },
+  {
+    name: "collection",
+    title: "Коллекция",
+  },
+  {
+    name: "newRelease",
+    title: "Новые релизы",
+  },
+];
 
 export const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   const CustomAppBar = styled(AppBar)`
@@ -43,6 +38,20 @@ export const Navbar = () => {
     display: flex;
     justify-content: center;
     align-items: center;
+  `;
+
+  const CustomNavLink = styled(NavLink)`
+    color: hsla(0, 0%, 100%, 0.5);
+    text-decoration: none;
+    text-transform: none;
+    font-size: 20px;
+    transition: 0.2s;
+    &.active {
+      color: white;
+    }
+    &:hover {
+      color: #ff4810;
+    }
   `;
 
   return (
@@ -82,48 +91,18 @@ export const Navbar = () => {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.title}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  textTransform: "none",
-                  my: 2,
-                  color: "white",
-                  fontSize: "20px",
-                  display: "block",
-                }}
-              >
-                {page}
+              <Button key={page.name} onClick={handleCloseNavMenu}>
+                <CustomNavLink to={`/${page.name}`}>{page.title}</CustomNavLink>
               </Button>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              anchorEl={anchorElUser}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
