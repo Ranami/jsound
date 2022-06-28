f058202cfc53feeaa719a63910def0b5025cf1f0
Ranami <nasyrov.ramir@mail.ru>
2022-06-28 15:57:48 +0600

    add styles for audio player

--- src/components/CustomAudioPlayer.tsx
+++ src/components/CustomAudioPlayer.tsx
@@ -1,6 +1,6 @@
 import { styled } from "@mui/material";
 import React, { FC, useCallback, useEffect, useState } from "react";
-import AudioPlayer from "react-h5-audio-player";
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import "../styles/audioplayer.css";
 import { SongType } from "../types/songType";
@@ -11,6 +11,27 @@ const Wrapper = styled("div")`
   background-color: rgba(24, 24, 24, 0.8);
   max-width: 1440px;
   width: 100%;
+`;
+
+const Image = styled("div")`
+  margin-right: 10px;
+  & img {
+    width: 50px;
+  }
+`;
+
+const Title = styled("div")`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const InfoWrapper = styled("div")`
+  text-align: left;
+`;
+
+const SongWrapper = styled("div")`
+  display: flex;
+  align-items: center;
 `;
 
 type CustomAudioPlayerType = {
@@ -33,10 +54,23 @@ export const CustomAudioPlayer: FC<CustomAudioPlayerType> = ({ song }) => {
           backgroundColor: "#1B2430",
           color: "white",
         }}
+        customControlsSection={[
+          <SongWrapper>
+            <Image>
+              <img src={song?.img} alt={song?.title} />
+            </Image>
+            <InfoWrapper>
+              <Title> {song?.title}</Title>
+              <div> {song?.artist}</div>
+            </InfoWrapper>
+          </SongWrapper>,
+          RHAP_UI.MAIN_CONTROLS,
+          RHAP_UI.VOLUME_CONTROLS,
+        ]}
         onPlay={(e) => console.log("onPlay")}
         showSkipControls={true}
         showJumpControls={false}
-        header={`${song?.artist} - ${song?.title}`}
+        // header={`${song?.artist} - ${song?.title}`}
       />
     </Wrapper>
   );
